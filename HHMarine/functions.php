835b44603adab80b59be71853a5925f6110ca566
linda@reachmaine.com <linda@reachmaine.com>
2019-05-29 13:27:02 -0400

    playing with footer and negative margins

--- HHMarine/functions.php
+++ HHMarine/functions.php
@@ -6,6 +6,18 @@ add_action( 'wp_enqueue_scripts', 'divi__child_theme_enqueue_styles' );
 
 
 //you can add custom functions below this line:
+
+//* Add custom body class to the head
+add_filter( 'body_class', 'reach_body_class' );
+function reach_body_class( $classes ) {
+
+  if ( is_front_page( ) ) {
+		$classes[] = 'home';
+  }
+		return $classes;
+}
+
+
 //require_once(get_stylesheet_directory().'/custom/reach-CTAs.php');
 require_once(get_stylesheet_directory().'/custom/language.php');
 require_once(get_stylesheet_directory().'/custom/divi.php');
